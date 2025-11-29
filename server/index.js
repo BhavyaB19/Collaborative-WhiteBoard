6a42e0761ec7c1c0c6262a362e4ca05a1070a6cc
BhavyaB19 <bhavyabisht345@gmail.com>
2025-11-29 12:56:53 +0530

    ...

--- server/index.js
+++ server/index.js
@@ -14,10 +14,10 @@ const app = express();
 const server = http.createServer(app);
 
 const allowedOrigins = [process.env.CLIENT_URL || 'http://localhost:5173']
-
+    
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({credentials: true, origin: "*"}));
+app.use(cors({credentials: true, origin: allowedOrigins}));
 
 attachSocket(server, { corsOrigin: allowedOrigins[0] });
 
